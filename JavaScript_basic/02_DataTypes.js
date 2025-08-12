a028e1a81a1db42dbefda5317be280349c1aa25b
Raman-Arora-74 <ramandeepsingharora91@gmail.com>
2025-08-12 16:24:47 +0530

    Data types of JavaScript

---
+++ JavaScript_basic/02_DataTypes.js
@@ -0,0 +1,32 @@
+//Data Types 
+
+"use strict"; // treat all Js code as newer version 
+
+// alert( 3 + 3 )  we are using nodejs , not browser 
+
+console.log(3 
+    +
+     3 ) // code readability should be high 
+
+console.log("Raman")
+
+let name = "Raman"
+let age = 16
+let isLoggedIn = true 
+
+// number => 2 to power 53
+// bigint to store more data 
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined => the value is not defined 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof(name)) // typeof is a build in function which tell the type of variable 
+console.log(typeof name) // we can right into both format 
+
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object
