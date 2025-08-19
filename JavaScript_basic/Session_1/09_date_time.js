6047aa0e01ab3c1ff057cf40ad053834c4cf41e4
Raman-Arora-76 <ramandeepsingharora91@gmail.com>
2025-08-19 22:23:16 +0530

    new files add

---
+++ JavaScript_basic/Session_1/09_date_time.js
@@ -0,0 +1,26 @@
+// Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// let myCreateDate = new Date(2023,0,23);
+
+// console.log(myCreateDate.toDateString());
+let myCreateDate2 = new Date("2023-01-14");
+// console.log(myCreateDate2.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate2.getTime());
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default',{
+    weekday:"long"
+}))
