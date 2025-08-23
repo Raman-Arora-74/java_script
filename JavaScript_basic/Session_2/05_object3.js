3aca6fb1478dcc4505e4c1e6a44081321e7d3afc
Raman-Arora-76 <ramandeepsingharora91@gmail.com>
2025-08-23 18:32:20 +0530

    object file added

---
+++ JavaScript_basic/Session_2/05_object3.js
@@ -0,0 +1,9 @@
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"htesh"
+}
+// couse.courseInstructor
+
+const {courseInstructor} = course 
+console.log(courseInstructor);
