20ff67d984a5c04da2dfc6f5ed6e6d58cd790ce2
Raman-Arora-76 <ramandeepsingharora91@gmail.com>
2025-09-02 22:45:42 +0530

    addition of new file and make it session 5

---
+++ JavaScript_basic/Session_5/for_loop.js
@@ -0,0 +1,62 @@
+// for 
+const num = 5 
+
+// for (let i = 0; i < 5; i++) {
+//     const element = i;
+//     console.log(element);
+    
+
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 5; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+//     }
+//     console.log(`Fetched Page ${i}`);
+    
+    
+// }
+// let myArray = ["Shaktiman","SuperMan","Flash","Batman"]
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+    
+// }
+
+// let myArray = ["Shaktiman","SuperMan","Flash","Batman"]
+// for (let i = myArray.length-1; i > -1; i--) {
+//     const element = myArray[i];
+//     console.log(element)
+    
+// }
+
+// break and continue
+
+// for (let i = 1; i <=20; i++) {
+//     if (i%5===0) {
+//         console.log(`Page shifted to next page ${i}`)
+//     }
+//     console.log(`I value ${i}`)
+    
+// }
+// break 
+// for (let i = 1; i <=20; i++) {
+//     if (i === 5 ) {
+//         console.log(`Detected 5`)
+//         break 
+//     }
+//     console.log(`I value ${i}`)
+    
+// }
+
+// continue
+
+for (let i = 1; i <=20; i++) {
+    if (i === 5 ) {
+        console.log(`Detected 5`)
+        continue 
+    }
+    console.log(`I value ${i}`)
+    
+}
---
+++ JavaScript_basic/Session_5/while_loop.js
@@ -0,0 +1,30 @@
+// while 
+// num  = 6
+// let i = 1 
+// while (i < num ) {
+//     console.log(`This is the value of I ${i}`);
+//     i++
+// }
+// let index  = 0 
+// while (index%2 === 0 ) {
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2;
+//     if (index === 32) {
+//         index= index + 1 
+//     } 
+    
+// }
+
+// let myArray = ['flash',"batman","Superman"]
+// let index = 0 
+// while (index < myArray.length) {
+//     console.log(`My Favorite hero ${myArray.at(index)}`)
+//     index = index + 1 
+// }
+// do while loop 
+// let score = 1 
+
+// do {
+//     console.log(`Score is ${score}`)
+//     score = score + 1 
+// } while(score <= 10 );
